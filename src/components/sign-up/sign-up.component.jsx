d4c459822b924a9d21089694771caa88b4a752cd
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-11-09 00:14:17 -0500

    Converts SignUp to class component
    Adds basic state and function structure for handling change and submit.

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import "./sign-up.styles.scss";
 
-const SignUp = ({ props }) => (
-  <div className="sign-up">
-    <h2>Create an Account</h2>
-    <span>Sign up with Google or by email</span>
-    <form>
-      <FormInput name="email" />
-      <FormInput name="password" />
-      <div className="buttons">
-        <CustomButton type="submit"> Sign Up </CustomButton>
-        <CustomButton type="submit"> Sign Up With Google </CustomButton>
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-up">
+        <h2>Create an Account</h2>
+        <span>Sign up with Google or by email</span>
+
+        <form onSubmit={this.handleSubmit}>
+
+          <FormInput 
+          name="email"
+          onChange={this.handleChange} />
+          <FormInput 
+          name="password" 
+          onChange={this.handleChange} />
+          
+          <div className="buttons">
+            <CustomButton type="submit" > Sign Up </CustomButton>
+            <CustomButton type="submit"> Sign Up With Google </CustomButton>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-);
+    );
+  }
+}
 
 export default SignUp;
