98607d07d215ba81bc9c657a460e51b59ec40365
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-11-08 23:47:22 -0500

    Creates basic signup component layout
    Does not include functionality, only basic layout.

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import FormInput from '../form-input/form-input.component'
-import CustomButton from '../custom-button/custom-button.component'
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
-import './sign-up.styles.scss'
+import "./sign-up.styles.scss";
 
 const SignUp = ({ props }) => (
+  <div className="sign-up">
+    <h2>Create an Account</h2>
+    <span>Sign up with Google or by email</span>
+    <form>
+      <FormInput name="email" />
+      <FormInput name="password" />
+      <div className="buttons">
+        <CustomButton type="submit"> Sign Up </CustomButton>
+        <CustomButton type="submit"> Sign Up With Google </CustomButton>
+      </div>
+    </form>
+  </div>
+);
 
-  <div>
-      
-      <CustomButton type="submit"> Sign Up </CustomButton>
-    </div>
-
-)
-
-
-export default SignUp
+export default SignUp;
