837601e39b4e63a259c82790844242d67405d895
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-11-10 20:28:44 -0500

    Updates button type

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,28 +9,28 @@ import CustomButton from '../custom-button/custom-button.component'
 import './header.styles.scss'
 
 const Header = ({ currentUser }) => (
-    
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-        </Link>
-      <CustomButton onClick={() => console.log(currentUser)} > Log State </CustomButton>
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <CustomButton onClick={() => console.log(currentUser)}>
+      Log State
+    </CustomButton>
 
-       
-        <div className='options'>
-            <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='/shop'>CONTACT</Link>
-            {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}> SIGN OUT </div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
-
-        </div>
-
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+        {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        ) : (
+        <Link className='option' to='/signin'>SIGN IN</Link>
+      )}
     </div>
-
+  </div>
 )
 
 export default Header
