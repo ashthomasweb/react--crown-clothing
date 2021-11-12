f0efe15e985b37bd08c50eae43f0488bf04e4126
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-11-11 21:39:47 -0500

    Added redux and built reducers
    User and root reducer created, props sent to header component.

--- src/App.js
+++ src/App.js
@@ -1,77 +1,76 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-import "./App.css";
+import './App.css'
 
-import HomePage from "./pages/homepage/home.component";
-import ShopPage from "./pages/shop/shop.component";
-import SignInUpPage from "./pages/signinuppage/signinuppage.component";
+import HomePage from './pages/homepage/home.component'
+import ShopPage from './pages/shop/shop.component'
+import SignInUpPage from './pages/signinuppage/signinuppage.component'
 import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import CustomButton from "./components/custom-button/custom-button.component"
+import { connect } from 'react-redux'
+import { setCurrentUser } from './redux/user/user.action'
+
 
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
   </div>
-);
+)
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      currentUser: null
-    }
-
-  }
 
   unsubscribeFromAuth = null
-  
+
   componentDidMount() {
-    // auth is object from firebase method, onAuthStateChanged is a subscription watcher 
+    const { setCurrentUser } = this.props
+
+    // auth is object from firebase method, onAuthStateChanged is a subscription watcher
     // userAuth is the object returned
-    auth.onAuthStateChanged(async userAuth => {
+    auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
 
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
+        userRef.onSnapshot((snapShot) => {
+
+          setCurrentUser({
+                id: snapShot.id,
+                ...snapShot.data(),
+              })
+            },
+
+            () => {
+              console.log(this.state)
             }
-          }, () => {
-            console.log(this.state)
-          })
-        })
+          )
       } else {
-        this.setState({ currentUser: userAuth })
+        setCurrentUser(userAuth)
       }
     })
   }
-
- 
 
   componentWillUnmount() {
     this.unsubscribeFromAuth()
   }
 
   render() {
-
     return (
       <div>
-      <Header className='App-header' currentUser={this.state.currentUser}/>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInUpPage} />
-        
-      </Switch>
+        <Header className='App-header' />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInUpPage} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+
+export default connect(null, mapDispatchToProps)(App)
